22b2e687853a46a6207d25f3dd4a8416a2cfc133
Alexei Torunov <merisho@ukr.net>
2018-02-28 14:06:43 +0200

    Dockerfile: run clustered application on container startup; add JSDoc for FilterRegistry over UDP

--- eventbus/FilterRegistry/udp/Client.js
+++ eventbus/FilterRegistry/udp/Client.js
@@ -3,6 +3,10 @@ const udpSocket = require('./udpSocket');
 const IFilterRegistry = require(`../IFilterRegistry`);
 
 class Client extends IFilterRegistry {
+    /**
+     * Creates new Filter Registry Client
+     * @param port Server port
+     */
     constructor(port) {
         super();
 
@@ -11,22 +15,47 @@ class Client extends IFilterRegistry {
         this._host = 'localhost';
     }
 
+    /**
+     * Registers new subscriber
+     * @param filter
+     * @param subscriber
+     */
     register(filter, subscriber) {
         return this._request('register', { filter, subscriber });
     }
 
+    /**
+     * Unregisters subscriber
+     * @param subscriber
+     */
     unregister(subscriber) {
         return this._request('unregister', { subscriber });
     }
 
+    /**
+     * Returns subscribers by filter
+     * @param filter
+     * @returns {Promise<Array<Subscriber>>}
+     */
     getSubscribers(filter) {
         return this._request('getSubscribers', { filter }).then(res => res.subscribers);
     }
 
+    /**
+     * Unregisters device subscription
+     * @param device
+     */
     unregisterDevice(device) {
         return this._request('unregisterDevice', { device });
     }
 
+    /**
+     * Send request over UDP to server
+     * @param method
+     * @param [params={}]
+     * @returns {Promise<*>}
+     * @private
+     */
     _request(method, params = {}) {
         return this._socket.request({ method, ...params }, this._port, this._host);
     }
